f7716f4ce19579f3cc0a03abdfa19e49cae55f7c
Agnes <133767448+agnes-vj@users.noreply.github.com>
2025-01-20 12:06:45 +0000

    Update Rock Paper Scissors game script

--- 1-dom-practice/script.js
+++ 1-dom-practice/script.js
@@ -18,3 +18,16 @@ appendButton.addEventListener("click", function(){
     newNode.innerText = "This heading was added dynamically by clicking a button!";
     document.body.appendChild(newNode);
 })
+
+const deleteFirstHeading = document.getElementById("deleteFirstHeading");
+deleteFirstHeading.addEventListener("click", function(){
+const firstH2Element = document.querySelector("h2");
+document.body.removeChild(firstH2Element);
+});
+
+
+const deleteAllHeading = document.getElementById("deleteAllHeadings");
+deleteAllHeading.addEventListener("click", function(){
+    const allH2Elements = document.querySelectorAll("h2")
+                                  .forEach(e =>  document.body.removeChild(e))   
+});
--- 2-paper-scissors-rock/index.js
+++ 2-paper-scissors-rock/index.js
@@ -37,10 +37,55 @@ const getRandomMove = () => {
   };
   
   const updateDOM = (moveOne, moveTwo, outcome) => {
-    // TODO Implement this method to update the DOM
-    // There are some images you can use in the images directory
+     displayPlayerMove("player1",moveOne);
+     displayPlayerMove("player2",moveTwo);    
+     addOutcome(outcome);
   };
-  
+const displayPlayerMove = (player, move) => {
+ 
+  var imageTag = null;
+  if (player === "player1")
+  {
+    const playerOneMove = document.getElementById("player-one-move__name");
+    playerOneMove.innerText = move;
+    imageTag = document.getElementById("player-one-move__img");
+  }
+  if (player === "player2")
+   {
+      const playerTwoMove = document.getElementById("player-two-move__name");
+      playerTwoMove.innerText = move;
+      imageTag = document.getElementById("player-two-move__img");
+  }
+ 
+    if (move === "scissors")
+      imageTag.src = "./images/scissors.png";
+    if (move === "rock")
+     imageTag.src = "./images/rock.png";
+    if (move === "paper")
+      imageTag.src = "./images/paper.png"; 
+}
+
+const displayPlayerTwoMove = (moveTwo) => {
+  const imageTag = document.getElementById("player-two-move__img");
+  if (moveTwo === "scissors")
+     imageTag.src = "./images/scissors.png";
+  if (moveOne === "rock")
+    imageTag.src = "./images/rock.png";
+  if (moveOne === "paper")
+    imageTag.src = "./images/paper.png"; 
+
+  const playerOneMove = document.getElementById("player-one-move__name");
+  playerOneMove.innerText = moveOne;
+
+}
+
+  const addOutcome = (outcome) => {
+    
+    const outcomeElement = document.createElement("h2")
+    outcomeElement.id = "outcome";
+    outcomeElement.innerText = outcome;    
+    document.body.appendChild(outcomeElement);
+  };
   const playButton = document.getElementById("play-btn");
   playButton.addEventListener("click", playGame);
   
