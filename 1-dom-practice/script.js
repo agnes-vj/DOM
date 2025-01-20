668854758531423f2cd742d40ede58305f8f88ee
sz-99 <zhengsusan3@gmail.com>
2025-01-20 10:48:32 +0000

    append child item to body

--- 1-dom-practice/script.js
+++ 1-dom-practice/script.js
@@ -6,7 +6,15 @@ paragraph.addEventListener("mouseover", function(){
 	paragraph.innerText = "mouse is hovering over...";
     paragraph.style.color = "red";
 })
+
 const button = document.getElementById("changeContentButton");
 button.addEventListener("click", function(){
     alert("You clicked a button!");
+})
+
+const appendButton = document.getElementById("appendButton");
+appendButton.addEventListener("click", function(){
+    const newNode = document.createElement("h2");
+    newNode.innerText = "This heading was added dynamically by clicking a button!";
+    document.body.appendChild(newNode);
 })
