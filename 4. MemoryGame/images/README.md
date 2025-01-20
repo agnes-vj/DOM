976a8dec927059d80152528b6151ebcc8929da9e
Agnes <133767448+agnes-vj@users.noreply.github.com>
2025-01-20 15:42:38 +0000

    add Memory Game

---
+++ 4. MemoryGame/images/README.md
@@ -0,0 +1,3 @@
+# C# - Intro to the DOM
+
+Instructions for today's sprint can be found at [https://l2c.northcoders.com/courses/cs-front-end/cs-dom](https://l2c.northcoders.com/courses/cs-front-end/cs-dom)
---
+++ 4. MemoryGame/index.js
@@ -0,0 +1,9 @@
+// const getRandom = () => {
+//     const pics = ["apple", "apple1", "scissors"];
+//     const randomIndex = Math.floor(Math.random() * 12);
+//     return moves[randomIndex];
+//   };
+// const reset = () =>{
+//     document.getElementsByClassName("item1")
+
+// };
