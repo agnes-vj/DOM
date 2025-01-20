078b9ccbe3e95adce38189d47194f206e33273a7
sz-99 <zhengsusan3@gmail.com>
2025-01-20 14:45:07 +0000

    complete implementation of password checker

--- 3-password/index.js
+++ 3-password/index.js
@@ -1,6 +1,30 @@
 const passwordInput = document.getElementById("passwordInput");
-passwordInput.addEventListener("input", function(){
-    console.log("password input")
+passwordInput.addEventListener("input", function(event){
+
+    var length = event.target.value.length
     var progressBar = document.getElementById("progressBar");
-    progressBar.value += 5;
+    var progressDesc = document.getElementById("progressDescription");
+
+
+    progressBar.style.accentColor = "red";
+    progressDesc.innerText = "Should be longer";
+    progressBar.value = length * 5;
+
+    if(length > 8 && length < 12)
+        {progressBar.style.accentColor = "orange";
+            progressDesc.innerText = "Pretty good";
+        };
+    if(length >= 12) {
+        progressBar.style.accentColor = "green";
+        progressDesc.innerText = "Grrrreat!";
+    };
+
+    
+    
+
+
 })
+
+
+
+
