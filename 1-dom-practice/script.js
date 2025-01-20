16f2b1f2539f4510b1ea0291c199655ea776dd42
Agnes <133767448+agnes-vj@users.noreply.github.com>
2025-01-20 10:28:22 +0000

    update script.js with paragraph and button name

--- 1-dom-practice/script.js
+++ 1-dom-practice/script.js
@@ -1,1 +1,4 @@
-console.log("Welcome to DOM");
+const paragraph = document.getElementById("targetParagraph");
+console.log("Paragraph Content : " + paragraph.innerText);
+const button = document.getElementById("changeContentButton");
+console.log("Button Name : " + button.innerText);
